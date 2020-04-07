2c97ec4fc6796ae9fa116ce41040d783107d83ed
Oran <shichman@post.bgu.ac.il>
2020-04-07 12:15:13 +0300

    Start Display team

--- System/src/main/java/Game/Team.java
+++ System/src/main/java/Game/Team.java
@@ -6,11 +6,34 @@ import java.util.List;
 
 public class Team {
 
+    private String teamName;
     private List<Player> teamPlayers;
     private List<Coach> teamCoaches;
     private List<TeamManager> teamManagers;
 
 
+    /**
+     * A method that returns a string containing all team information for display
+     * @return string containing all team information.
+     */
+    public String DisplayTeam(){
+        String output = "Team Name:" + teamName + "\n" +
+                        "Team Coaches:";
+
+
+
+        return output;
+    }
+
+    @Override
+    public String toString() {
+        return "Team{" +
+                "teamName='" + teamName + '\'' + "\n" +
+                ", teamPlayers=" + teamPlayers +"\n" +
+                ", teamCoaches=" + teamCoaches +"\n" +
+                ", teamManagers=" + teamManagers +"\n" +
+                '}';
+    }
 
     public boolean addTeamPlayer(Registered teamPlayer)
     {
