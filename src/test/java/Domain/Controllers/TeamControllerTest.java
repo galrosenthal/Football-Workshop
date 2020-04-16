c0acd38dc1c345b54391e61a112b888d1eb3cff0
Oran <shichman@post.bgu.ac.il>
2020-04-16 12:22:17 +0300

    Starting testing

--- src/test/java/Domain/Controllers/TeamControllerTest.java
+++ src/test/java/Domain/Controllers/TeamControllerTest.java
@@ -1,9 +1,20 @@
 package Domain.Controllers;
 
+import Domain.Game.Team;
+import Domain.Users.RoleTypes;
+import Domain.Users.SystemUser;
+import Domain.Users.TeamOwner;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TeamControllerTest {
 
+    @Test
+    public void testAddTeamOwner() throws Exception {
+        boolean result = TeamController.addTeamOwner("username", new Team(), new TeamOwner(new SystemUser("username", null, "name")));
+        Assert.assertEquals(false, result);
 
-
+    }
 }
+
+//Generated with love by TestMe :) Please report issues and submit feature requests at: http://weirddev.com/forum#!/testme
