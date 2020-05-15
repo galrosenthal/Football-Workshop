c9dc17b1b4e1396b4878e06991628eb9df720894
Gal <galrosenthal@gmail.com>
2020-05-15 16:02:56 +0300

    fixed all test issues

--- src/main/java/Domain/Users/Coach.java
+++ src/main/java/Domain/Users/Coach.java
@@ -110,7 +110,7 @@ public class Coach extends PartOfTeam {
         String valueOfProperty = "";
         if(property.equalsIgnoreCase(teamJobString))
         {
-            valueOfProperty = UIController.receiveString("what is the Coach JobTitle?", null);
+            valueOfProperty = UIController.receiveString("what is the Coach JobTitle?");
         }
         else if(property.equalsIgnoreCase(qualificationString))
         {
