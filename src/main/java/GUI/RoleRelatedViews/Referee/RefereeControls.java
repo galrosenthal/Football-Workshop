6cbf5dd3aa93b7fd8de9cb511cad36cca2b3e8ca
Oran <shichman@post.bgu.ac.il>
2020-05-15 19:26:13 +0300

    -

--- src/main/java/GUI/RoleRelatedViews/Referee/RefereeControls.java
+++ src/main/java/GUI/RoleRelatedViews/Referee/RefereeControls.java
@@ -1,9 +1,14 @@
 package GUI.RoleRelatedViews.Referee;
 
 import GUI.FootballMain;
+import Service.MainController;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.VaadinService;
+import com.vaadin.flow.server.VaadinSession;
 
 @Route(value = "REF", layout = FootballMain.class)
 @PageTitle("Referee Controls")
@@ -19,8 +24,33 @@ public class RefereeControls extends VerticalLayout {
 
     private void buildUI() {
         createShowActiveGameButton();
+        createDisplayActiveGame();
+    }
+
+    private void createDisplayActiveGame() {
+        String buttonTaskName = "Display Active Game";
+        Button watchGamesBtn = new Button(buttonTaskName);
+        watchGamesBtn.addClickListener(e -> {
+
+            UI lastUI = UI.getCurrent();
+            VaadinSession se = VaadinSession.getCurrent();
+            String username = (String) VaadinService.getCurrentRequest().getWrappedSession().getAttribute("username");
+            Thread t = new Thread(() -> {
+                UI.setCurrent(lastUI);
+                VaadinSession.setCurrent(se);
+                MainController.assignReferee(username);
+            });
+            t.setName(buttonTaskName.toUpperCase());
+            t.start();
+
+        });
+
+        add(watchGamesBtn);
     }
 
     private void createShowActiveGameButton() {
+        String buttonTaskName = "Watch games you scheduled";
+        Button watchGamesBtn = new Button(buttonTaskName);
+
     }
 }
