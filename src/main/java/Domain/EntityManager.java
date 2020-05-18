d188897a838f477fed4e1be99f8232dca51d29b5
Nir <nirdz@post.bgu.ac.il>
2020-05-18 18:47:31 +0300

    Hashed the system user's passwords

--- src/main/java/Domain/EntityManager.java
+++ src/main/java/Domain/EntityManager.java
@@ -381,7 +381,9 @@ public class EntityManager {
      * @return true if the password is correct and the user is able to login
      */
     private boolean authenticate(SystemUser userWithUsrNm, String pswrd) {
-        if (userWithUsrNm.getPassword().equals(pswrd)) {
+        //hash the given password
+        String hashedPassword = org.apache.commons.codec.digest.DigestUtils.sha256Hex(pswrd);
+        if (userWithUsrNm.getPassword().equals(hashedPassword)) {
             return true;
         }
         return false;
@@ -421,7 +423,9 @@ public class EntityManager {
             throw new WeakPasswordException("Password does not meet the requirements");
         }
 
-        SystemUser newUser = new SystemUser(usrNm, pswrd, name);
+        //hash the password
+        String hashedPassword = org.apache.commons.codec.digest.DigestUtils.sha256Hex(pswrd);
+        SystemUser newUser = new SystemUser(usrNm, hashedPassword, name);
         addUser(newUser);
 
 
