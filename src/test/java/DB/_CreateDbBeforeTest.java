1aee1d3d2add82b16162c248f719075b4e30ee9b
Gal <galrosenthal@gmail.com>
2020-05-29 17:34:26 +0300

    trying only db connection

--- src/test/java/DB/_CreateDbBeforeTest.java
+++ src/test/java/DB/_CreateDbBeforeTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class DBHandlerTesting {
+public class _CreateDbBeforeTest {
 
     DBHandler db;
 
@@ -13,7 +13,8 @@ public class DBHandlerTesting {
     public void DBHandlerConnectionTesting()
     {
          DBHandler.startConnection("jdbc:mysql://localhost:3306/fwdb_test");
-         DBHandler.getInstance().create();
+//         DBHandler.getInstance().create();
     }
+
 
 }
