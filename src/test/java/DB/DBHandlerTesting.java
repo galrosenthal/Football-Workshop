828aa70b593ec2b93fae460b0f84e8f4f991cef8
Gal <galrosenthal@gmail.com>
2020-05-29 16:31:58 +0300

    trying only db connection

---
+++ src/test/java/DB/DBHandlerTesting.java
@@ -0,0 +1,19 @@
+package DB;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DBHandlerTesting {
+
+    DBHandler db;
+
+    @Test
+    public void DBHandlerConnectionTesting()
+    {
+         DBHandler.startConnection("jdbc:mysql://localhost:3306/fwdb_test");
+         DBHandler.getInstance().create();
+    }
+
+}
